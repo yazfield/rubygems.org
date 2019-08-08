f1b0c093da49295b57eac29d0f49ae9154f611d9
shunshunni <shun_nishitani@me.com>
2019-08-09 01:53:35 +0900

    refactor autocomplete javascript

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,34 +1,85 @@
-$(function(){
+$(function() {
   $('#home_query').autocomplete();
-  $('.header__search').autocomplete();
+  $('#query').autocomplete();
 });
 
 
-(function($){
+(function($) {
   $.fn.autocomplete = function() {
     var indexNumber = -1;
     var previousForm = 'null';
     var listName = this.attr('id') + "-suggestList";
     var that = this;
+    var form = 'null';
     this.attr('autocomplete', 'off');
     this.after("<ul id=" + listName + "></ul>");
     var list = $('#' + listName);
     list.attr('class', 'suggest-list');
 
-    this.blur(function(){
+    function addDataToSuggestList(data) {
+      list.find('li').remove();
+      for (var i = 0; i < data.length && i < 10; i++) {
+        var newLi = $('<li>').text(data[i]);
+        $(newLi).attr('class', 'menu-item');
+        list.append(newLi);
+      }
+      indexNumber = -1;
+      list.show();
+    };
+
+    $.fn.focusedItem = function() {
+      $('li').removeClass('selected');
+      this.addClass('selected');
+    };
+
+    function move(dir) {
+      switch (dir) {
+        case 'prev':
+          indexNumber--;
+          break;
+        case 'next':
+          indexNumber++;
+          break;
+      };
+      switch (indexNumber) {
+        case -1:
+        case -2:
+          indexNumber = list.find('li').length - 1;
+          break;
+        case list.find('li').length:
+          indexNumber = 0;
+          break;
+      };
+    };
+
+    function arrowSelect(arrow) {
+      list.find('li').removeClass('selected');
+      switch (arrow) {
+        case 'up':
+          move('prev');
+          break;
+        case 'down':
+          move('next')
+          break;
+      };
+      list.find('li').eq(indexNumber).focusedItem();
+      that.val(list.find('.selected').text());
+      list.show();
+    };
+
+    this.blur(function() {
       list.hide();
     });
-    this.focus(function(){
-      if (!list.find('li').length){
+    this.focus(function() {
+      if (!list.find('li').length) {
         list.hide();
       } else {
         list.show();
       };
     });
 
-    this.keyup(function(e){
+    this.keyup(function(e) {
       e.preventDefault();
-
       var input = $.trim($(this).val());
       if (input.length >= 2 && previousForm != input) {
         $.ajax({
@@ -40,36 +91,16 @@ $(function(){
           dataType: 'json'
         }).done(function(data) {
           if (data.length) {
-            list.find('li').remove();
-            for (var i = 0; i < data.length && i < 10; i++) {
-              var newLi = $('<li>').text(data[i]);
-              $(newLi).attr('class', 'menu-item');
-              list.append(newLi);
-            }
-            indexNumber = -1;
-            list.show();
+            addDataToSuggestList(data);
           } else {
             list.hide();
           };
         });
       };
-
-      if (e.keyCode == 40) {
-        list.find('li').removeClass('selected');
-        indexNumber += 1;
-        if (indexNumber > (list.find('li').length - 1)) {
-          indexNumber %= (list.find('li').length);
-        };
-        list.find('li').eq(indexNumber).mouseenter();
-        list.show();
-      } else if (e.keyCode == 38) {
-        list.find('li').removeClass('selected');
-        indexNumber -= 1;
-        if (indexNumber < (list.find('li').length - 1)) {
-          indexNumber %= (list.find('li').length);
-        };
-        list.find('li').eq(indexNumber).mouseenter();
-        list.show();
+      if (e.keyCode == 38) {
+        arrowSelect('up');
+      } else if (e.keyCode == 40) {
+        arrowSelect('down');
       };
       if (input.length < 2) {
         list.hide();
@@ -83,19 +114,16 @@ $(function(){
 
     $(document).on({
       'mousedown': function() {
-        setTimeout(function() {
-          that.focus();
-        }, 0);
+        that.val($(this).text())
+        that.parent().submit();
       },
       'mouseenter': function() {
-        list.find('li').removeClass('selected');
-        $(this).addClass('selected');
-        that.val($(this).text());
-        previousForm = that.val();
+        $(this).focusedItem();
+        indexNumber = $('.selected').index();
       },
       'mouseleave': function() {
         $(this).removeClass('selected');
       }
-    }, '.menu-item');
+    }, ('#' + listName + ' > .menu-item'));
   };
 })(jQuery);
